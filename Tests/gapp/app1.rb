4600747419f3bb4b4cd08298c0fd2e1e09a0f4ab
Cecil <ccoupe@cableone.net>
2018-03-02 20:26:58 -0700

    tweak test #400

--- Tests/gapp/app1.rb
+++ Tests/gapp/app1.rb
@@ -3,7 +3,7 @@ Shoes.app do
     para "Check if we can access shoes.yaml globals"
     button "Check" do
       @st = Shoes.settings
-      @eb.append "Have settings for #{@st.inspect}\n"
+      @eb.append "Have settings for #{@st}\n"
     end 
     @eb = edit_box width: 400, height: 300
   end
